fff391f92e6c7c4015f677b89a5b70e59de6ba2a
bobinlee <cultist_tp@naver.com>
2018-03-16 18:03:45 +0900

    유알엘 수정.

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,6 +1,6 @@
 const data = {
     y2048: "https://drive.google.com/open?id=1IpBPlsVddgxdhu6NPuYdEzfbfxVAn8kX",
-    aroma: "https://drive.google.com/open?id=1tVlNHcNTR9UlzzTysFY7qSIH1j3unrm6",
+    aroma: "https://drive.google.com/open?id=1jB50YNi_Z8qNFj0RNa1jLZZ5tM6SabWE",
     asiana: "https://drive.google.com/open?id=1rEwhrrjq5mzWd8x1bFeWMODjvIU9HeYp",
     pidget: "https://drive.google.com/file/d/1IpBPlsVddgxdhu6NPuYdEzfbfxVAn8kX"
 };
