9812425132bd88a9513c3133e0307c5c206e2037
bobinlee <cultist_tp@naver.com>
2018-03-16 17:48:41 +0900

    유알엘 수정.

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,7 +1,7 @@
 const data = {
     y2048: "https://drive.google.com/open?id=1IpBPlsVddgxdhu6NPuYdEzfbfxVAn8kX",
     aroma: "https://drive.google.com/open?id=1tVlNHcNTR9UlzzTysFY7qSIH1j3unrm6",
-    asiana: "https://drive.google.com/open?id=1_RJJNLFecjgeGrgsm1C3kDw2pEfQIBQv",
+    asiana: "https://drive.google.com/open?id=1rEwhrrjq5mzWd8x1bFeWMODjvIU9HeYp",
     pidget: "https://drive.google.com/file/d/1IpBPlsVddgxdhu6NPuYdEzfbfxVAn8kX"
 };
 
